8feaa4afa52b249a63df3acdb64d83d779fd1771
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2022-02-11 19:14:35 +0300

    6

--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -63,8 +63,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        new_queryset = title.reviews.all()
-        return new_queryset
+        return title.reviews.all()
 
     def perform_create(self, serializer):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
@@ -80,8 +79,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         review = get_object_or_404(Review,
                                    title_id=self.kwargs.get('title_id'),
                                    id=self.kwargs.get('review_id'))
-        new_queryset = review.comments.all()
-        return new_queryset
+        return review.comments.all()
 
     def perform_create(self, serializer):
         review = get_object_or_404(Review,

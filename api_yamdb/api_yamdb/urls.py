bf250b4beecd9f532de5eb91269bc788b0cafd86
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2022-02-15 00:35:18 +0300

    1

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path(

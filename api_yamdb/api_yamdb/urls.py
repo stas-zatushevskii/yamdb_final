07511a30e2552469bb38fe57bf0a5ebffcee9e8e
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2022-02-16 20:03:18 +0300

    1

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -12,11 +11,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
 ]
 
 schema_view = get_schema_view(
